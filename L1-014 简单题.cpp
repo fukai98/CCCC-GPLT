6c39cc92881a4eadcb41fced71c37f176ef2cf38
fukai <fukai9809@163.com>
2019-08-15 16:39:28 +0800

    CCCC-GPLT😊

---
+++ L1-014 简单题.cpp
@@ -0,0 +1,16 @@
+/*
+L1-014 简单题 (5 分)
+这次真的没骗你 —— 这道超级简单的题目没有任何输入。
+你只需要在一行中输出事实：This is a simple problem. 就可以了。
+输入样例：
+无
+输出样例：
+This is a simple problem.
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "This is a simple problem.";
+    return 0;
+}
